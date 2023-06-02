e4e0830fb0a15e8082dbae22300fa60f351546e3
Michi-San <117457596+Michi-San@users.noreply.github.com>
2023-06-02 21:43:40 +0200

    Add files via upload

---
+++ cards/image.js
@@ -0,0 +1,28 @@
+const image = [
+  {
+    key: 1,
+    title: "Individuelle Bedarfsanalyse",
+    content:
+      "Im persönlichen Gespräch analysieren wir individuell mit Dir die Stärken Deines bestehenden Leistungsangebots."
+  },
+  {
+    key: 1,
+    title: "Individuelle Bedarfsanalyse",
+    content:
+      "Im persönlichen Gespräch analysieren wir individuell mit Dir die Stärken Deines bestehenden Leistungsangebots."
+  },
+  {
+    key: 1,
+    title: "Individuelle Bedarfsanalyse",
+    content:
+      "Im persönlichen Gespräch analysieren wir individuell mit Dir die Stärken Deines bestehenden Leistungsangebots."
+  },
+  {
+    key: 1,
+    title: "Individuelle Bedarfsanalyse",
+    content:
+      "Im persönlichen Gespräch analysieren wir individuell mit Dir die Stärken Deines bestehenden Leistungsangebots."
+  }
+];
+
+export default image;
---
+++ cards/src/card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function card(props) {
+  return (
+    <div class="card">
+      <h1 class="card-title">{props.title}</h1>
+      <h2 class="card-content">{props.content}</h2>
+    </div>
+  );
+}
+
+
+
+export default card;
